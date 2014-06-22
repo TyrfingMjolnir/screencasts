a83a2c693ce38b34a030d8ac293a6ed2a413ab32
Tyrfing Mjolner <4lphamal3@gmail.com>
2014-06-22 14:17:35 +0800

    Update app.js
    Added vim( h and l ) and doom( a and d ) keystrokes to next and previous.
    
    doomified

--- introToAngular/exampleViewer/app.js
+++ introToAngular/exampleViewer/app.js
@@ -1,12 +1,13 @@
-var app = angular.module('exampleViewerApp', ['ngRoute']);
+var app = angular.module( 'exampleViewerApp',
+  ['ngRoute'] );
 
-app.config(function($routeProvider) {
+app.config( function( $routeProvider ) {
   $routeProvider.
-    when('/', {
+    when( '/', {
       templateUrl: 'example-list.html',
       controller: 'ExampleListCtrl'
     }).
-    when('/:exampleNumber', {
+    when( '/:exampleNumber', {
       templateUrl: 'example-detail.html',
       controller: 'ExampleDetailCtrl'
     }).
@@ -15,74 +16,88 @@ app.config(function($routeProvider) {
     });
 });
 
-app.factory('examples', function($http){
-
-  function getData(callback){
+app.factory( 'examples', function( $http ) {
+  function getData( callback ) {
     $http({
       method: 'GET',
       url: '../examples.json',
       cache: true
-    }).success(callback);
+    }).success( callback );
   }
 
   return {
     list: getData,
-    find: function(exampleNumber, callback){
-      getData(function(data) {
-        var index = parseInt(exampleNumber) - 1;
-        callback(data[index]);
+    find: function( exampleNumber, callback ) {
+      getData( function( data ) {
+        var index = parseInt( exampleNumber ) - 1;
+        callback( data[index] );
       });
     }
   };
 });
 
 // Responsible for navigating based on key events.
-app.controller('MainCtrl', function ($scope, $document, $location, examples){
-  examples.list(function(examples){
+app.controller( 'MainCtrl', function( $scope, $document, $location, examples ) {
+  examples.list( function( examples ) {
     var LEFT = 37,
         RIGHT = 39;
 
-    $scope.changeExample = function(e) {
+    $scope.changeExample = function( e ) {
       var path = $location.path(),
           // The example number
           n;
 
       // If there is a number,
-      if(path.length > 1){
+      if( path.length > 1 ) {
 
         // Extract the example number from the path.
-        n = parseInt(path.substr(1), 10);
+        n = parseInt( path.substr( 1 ), 10 );
 
         // Increment or decrement the example number.
-        if(e.keyCode === RIGHT && n < examples.length){
-          n++;
-        } else if(e.keyCode === LEFT && n > 1) {
-          n--;
+        switch( e.keyCode ) {
+          // LEFT
+          case 37:
+          case 65:
+          case 72:
+            if( n > 1 ) {
+              n--;
+            }
+          break;
+          // RIGHT
+          case 39:
+          case 68:
+          case 76:
+            if( n < examples.length ) {
+              n++;
+            }
+          break;
+          default:
+            console.log( e.keyCode );
         }
         
         // Navigate to the previous or next example.
-        $location.path('/' + n);
+        $location.path( '/' + n );
       }
     };
   });
 });
 
-app.controller('ExampleListCtrl', function ($scope, examples){
-  examples.list(function(examples) {
+app.controller( 'ExampleListCtrl', function( $scope, examples ) {
+  examples.list( function( examples ) {
     $scope.examples = examples;
   });
 });
 
-app.controller('ExampleDetailCtrl',
-    function ($scope, $routeParams, $http, $sce, examples){
-  examples.find($routeParams.exampleNumber, function(example) {
+app.controller( 'ExampleDetailCtrl',
+    function( $scope, $routeParams, $http, $sce, examples ) {
+  examples.find( $routeParams.exampleNumber, function( example ) {
     $scope.example = example;
     var examplePath = '../examples/snapshots/' + example.name;
     $scope.runUrl = examplePath + '/index.html';
-    $http.get(examplePath + '/README.md').success(function(data) {
+    $http.get( examplePath + '/README.md' ).success( function( data ) {
       // Remove first line, as it appears elsewhere on the page (called 'message').
-      var md = data.split('\n').splice(1).join('\n');
-      $scope.readme = $sce.trustAsHtml(marked(md));
+      var md = data.split( '\n' ).splice( 1 ).join( '\n' );
+      $scope.readme = $sce.trustAsHtml( marked( md ) );
     });
   });
 });
@@ -92,20 +107,20 @@ app.controller('ExampleDetailCtrl',
  * example source code file into a CodeMirror instance
  * for syntax-highlighted presentation.
  */
-app.directive('file', function(){
+app.directive( 'file', function() {
   return {
     scope: { file: '=', example: '=' },
     restrict: 'A',
-    controller: function($scope, $http){
+    controller: function( $scope, $http ) {
       var path = [
         '../examples/snapshots',
         $scope.example.name,
         $scope.file
-      ].join('/');
-      $http.get(path).success(function(data) {
-        if(typeof(data) === 'object'){
+      ].join( '/' );
+      $http.get( path ).success( function( data ) {
+        if( typeof( data ) === 'object' ){
           // un-parse auto-parsed JSON files for presentation as text
-          data = JSON.stringify(data, null, 2);
+          data = JSON.stringify( data, null, 2 );
         } else {
           // Remove trailing newlines from code presentation
           data = data.trim();
@@ -113,16 +128,16 @@ app.directive('file', function(){
         $scope.content = data;
       });
     },
-    link : function(scope, element, attrs) {
+    link : function( scope, element, attrs ) {
       var textArea = element[0];
-      var editor = CodeMirror.fromTextArea(textArea, {
+      var editor = CodeMirror.fromTextArea( textArea, {
         mode: "text/html",
         lineNumbers: true,
         viewportMargin: Infinity
       });
-      scope.$watch('content', function(data){
-        if(data) {
-          editor.setValue(data);
+      scope.$watch( 'content', function( data ){
+        if( data ) {
+          editor.setValue( data );
         }
       });
     }
